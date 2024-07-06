370bc0178b2ca2152251bf0e345568fe13a62a2e
Márton Áron <aron17marton@gmail.com>
2024-07-06 16:36:05 +0200

    feat: v2024.7.6

--- app/src/main/java/com/sciencewolf/szakdolgozat/rpiapi/LED.kt
+++
@@ -1,6 +0,0 @@
-package com.sciencewolf.szakdolgozat.rpiapi
-
-data class LED(
-    val status_code: Int = 0,
-    val timestamp: String = ""
-)
---
+++ app/src/main/java/com/sciencewolf/szakdolgozat/scheme/LEDResponse.kt
@@ -0,0 +1,6 @@
+package com.sciencewolf.szakdolgozat.scheme
+
+data class LEDResponse(
+    val status_code: Int = 0,
+    val timestamp: String = ""
+)
